d95501fe6b514b27ae1b074dfdd1b02f98cfa2cb
sofiane <sofian.bekkari@outlook.com>
2022-03-04 14:21:24 +0100

    Added a class Model and Database

--- market.py
+++ market.py
@@ -1,10 +1,26 @@
 ## IMPORT SECTION
 
 from flask import Flask, render_template # MOST IMPORTENT
+from flask_sqlalchemy import SQLAlchemy # DATABASE
 
 ## END IMPORT SEC
 
 app = Flask(__name__) # MOST IMPORTENT APP 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db' # CONFIGURATION uri FOR FILE market.db
+db = SQLAlchemy(app) # SETTING UP DATABASE
+
+
+# MODEL DB CLASS
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    product = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    # FOR BETTER VIEW ITEM IN DB
+    def __repr__(self):
+        return f'Item {self.product}'
 
 @app.route("/")
 @app.route("/home")
@@ -13,11 +29,9 @@ def home_page():
 
 @app.route("/market")
 def market_page():
-    items = [
-    {'id': 1, 'name': 'Phone', 'barcode': 192734625, 'price': 50},
-    {'id': 2, 'name': 'Desktop', 'barcode': 192734625, 'price': 550},
-    {'id': 3, 'name': 'Laptop', 'barcode': 192734625, 'price': 350}
-    ]
+    # SHOW ITEM TABLE FROM DATABASE
+    items = Item.query.all() # ALL ITEMS
+    
     return render_template('market.html', items=items)
 
 
